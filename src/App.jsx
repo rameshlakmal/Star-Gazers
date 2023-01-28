22e15ddf52e0e5e3cefe8ad6b4fc8b8225f130ae
rameshlakmal <rameshslakmal1999@gmail.com>
2023-01-29 00:26:22 +0530

    combining useEffect with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import Welcome from "./components/Welcome";
 import Support from "./components/Support";
-
+import ListCast from "./components/ListCast";
 
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
           <img src="images/group.svg" alt="stargazes group" />
           <Welcome name="Star Gazers"/>
           <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <ListCast/>
           <Support/>
         </hgroup>
       </article>
