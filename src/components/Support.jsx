296f6e0f50cee99be5d519d619cd51bd36395927
rameshlakmal <rameshslakmal1999@gmail.com>
2023-01-28 23:46:27 +0530

    Using effects

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,9 +1,15 @@
-  import { useState } from "react";
+  import { useState, useEffect } from "react";
+
+  const pageTitle = document.title;
   
   
   
    export default ()=>{
     const [count, setCount] = useState(0);
+
+    useEffect(()=>{
+        count && (document.title = `${pageTitle}-${count}`)
+    })
 
     return(
         <button className="outline" onClick={()=>setCount(count + 1)}> 
