a2c59077331abf4cec1b91d11b492c0bc4f107ec
rameshlakmal <rameshslakmal1999@gmail.com>
2023-01-28 22:45:00 +0530

    classes and hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import React, {Component} from "react";
+
+class Welcome extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return <h1>Meet the StarGazers {this.props.name}</h1>
+  }
+}
+
+const Hello = (props)=>{
+  return <h1>Meet the <i>{props.name}</i></h1>
+}
+
+
+
+
 function App() {
   const name = 'Star Gazers'
   return (
@@ -5,7 +23,9 @@ function App() {
       <article>
         <hgroup>
           <img src="images/group.svg" alt="stargazes group" />
-          <h1>Meet the <i style={{color:"steelBlue"}}>{name}</i></h1>
+          <Welcome name="Stars"/>
+          <Hello name="Ramesh"/>
+          <Welcome name="Star Aliens"/>
           <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <button className="outline" onClick={()=>{alert('Hi there')}}>Click Me</button>
         </hgroup>
